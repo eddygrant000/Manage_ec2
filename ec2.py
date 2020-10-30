8b2cc5ecfd3ef498b8941f09a3ceed02cd00e68a
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2020-10-31 01:35:16 +0530

    Add files via upload

---
+++ ec2.py
@@ -0,0 +1,124 @@
+#!/bin/python3.7
+
+import boto3
+import sys
+from prettytable import PrettyTable
+
+ec2 = boto3.resource("ec2")
+
+## Default Values
+ubuntu16 = "ami-048bb32b1fb7c36b7"
+ubuntu20 = "ami-0cda377a1b884a1bc"
+rhel8 = "ami-052c08d70def0ac62"
+amazon = "ami-0e306788ff2473ccb"
+default_image = ubuntu20
+default_count = 1
+default_keyname = 'devops'
+default_sec_group = 'personal'
+default_type = 't2.micro'
+default_status = 'status'
+default_tag = 'Nodes'
+
+## Fetch Arguements
+def fetch_args():
+    global default_image, default_count, default_keyname, default_sec_group, default_type, default_status
+    all_args = sys.argv
+    try:
+        default_status = all_args.pop(1)
+    except:
+        print("Please provide Valid Input:\nExample:\n[Create | Stop | Status | Terminate]")
+    else:
+        for i in all_args:
+            if 'allport' == i.lower():
+                default_sec_group = 'all expose'
+            elif 'ubuntu16' == i.lower():
+                default_image = ubuntu16
+            elif 'rhel' == i.lower():
+                default_image = rhel8
+            elif 'amazon' == i.lower():
+                default_image = amazon
+            elif i.isdigit():
+                default_count = int(i)
+            elif 't2' in i.lower():
+                default_type = i.lower()
+
+
+## Create ec2 instance
+def create_instance():
+    instances = ec2.create_instances(
+        ImageId=default_image,
+        MinCount=default_count,
+        MaxCount=default_count,
+        InstanceType=default_type,
+        KeyName=default_keyname,
+        SecurityGroupIds=[default_sec_group,],
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags':[
+                    {
+                        'Key': 'Name',
+                        'Value': default_tag
+                    },]},]
+        )
+
+
+# Start instance
+def start_instance():
+    for i in ec2.instances.all():
+        if i.state["Name"] == 'stopped':
+            ec2.Instance(i.id).start()
+            print(f'{i.id} Start SuccessFully')
+            
+
+# create table for print
+def tableprint(field_names, rows):
+    table = PrettyTable()
+    table.field_names = field_names
+    table.align[field_names[0]] = 'l'
+    for row in rows:
+        table.add_row(row)
+    print(table)
+
+
+# status of instance
+def status_instance():
+    temp = []
+    fields = ("Name", "Status", "Type", "Public IP", "Private IP")
+    for instance in ec2.instances.all():
+        temp.append((instance.tags[0]["Value"], 
+                     instance.state["Name"], 
+                     instance.instance_type, 
+                     instance.public_ip_address,
+                     instance.private_ip_address,))
+    tableprint(fields, temp)
+
+
+# terminate instance
+def terminate_instance():
+    for i in ec2.instances.all():
+        if i.state['Name'] in ('running', 'stopped'):
+            ec2.Instance(i.id).terminate()
+            print(f'{i.id} Terminate SuccessFully')
+
+
+# stop instance
+def stop_instance():
+    for i in ec2.instances.all():
+        if i.state['Name'] == 'running':
+            ec2.Instance(i.id).stop()
+            print(f'{i.id} Stop SuccessFully')
+
+
+if __name__ == "__main__":
+    fetch_args()
+    if default_status.lower() == 'create':
+        create_instance()
+    elif default_status.lower() == 'status':
+        status_instance()
+    elif default_status.lower() == 'terminate':
+        terminate_instace()
+    elif default_status.lower() == 'stop':
+        stop_instance()
+    else:
+        print("Please provide Valid Input:\nExample:\n[Create | Stop | Status | Terminate]")
